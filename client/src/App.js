913f9539b8c7e5a7b8a2d333a110905cf94eb76d
NikitaVadadoriya <nikitavaddoriya0@gmail.com>
2023-07-29 15:18:47 +0530

    home

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Registration from './Registration'
 import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
 import Login from './Login'
+import Home from './Home'
 const App = () => {
   return (
    <Router>
     <Routes>
       <Route path='/' element={<Registration/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
+      <Route path='/home' element={<Home/>}></Route>
     </Routes>
    </Router> 
   )
