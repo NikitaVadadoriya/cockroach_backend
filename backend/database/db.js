246cd73170a0f574ac22b91590fe558b6e1e5013
NikitaVadadoriya <nikitavaddoriya0@gmail.com>
2023-07-29 14:57:26 +0530

    pgg

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize-cockroachdb");
 require("dotenv").config();
-
+const { Client } = require('pg');
 // const ca_cert = process.env.SSL_CERT ? process.env.SSL_CERT : false
 const db = new Sequelize(process.env.DATABASE_STRING,
     {
         dialectOptions: {
             application_name: "docs_simplecrud_node-sequelize",
-            dialect: 'mysql', 
+            dialect: 'mysql',
+            dialectModule: Client, 
         },
       
         logging: false
